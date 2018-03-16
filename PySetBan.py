453aedd9ac4ccd11b006b48e9299b09fe81697f8
Vlurk <denis.roy@gmail.com>
2018-03-15 21:49:34 -0400

    New option : filter on protocol version

--- PySetBan.py
+++ PySetBan.py
@@ -7,14 +7,13 @@ import subprocess
 from time import sleep
 
 
-version = '/Rhizocarpon:1.0.1.1/'
-
 parser = argparse.ArgumentParser(description='Ban the peers not running version X\nRequirement: wallet must support the setban command')
 parser.add_argument('--outfile', nargs='?',  type=str, help='output json file', default="")
 parser.add_argument('--infile', nargs='?', type=str, help='input json file', default="")
 parser.add_argument('-s', '--seconds', nargs='?', type=int, dest="seconds", default=86400, help='ban for this many seconds')
 parser.add_argument('-c', '--cli', nargs='?', type=str, dest="cli", default="numusd", help="wallet cli command")
-parser.add_argument('--subver', nargs='?', type=str, default='/Rhizocarpon:1.0.1.1/', help="Accept only this subversion string")
+parser.add_argument('--subver', nargs='?', type=str, default="", help="Accept only this subversion string")
+parser.add_argument('--protocol', nargs='?', type=str, default="", help="Accept only this protocol version")
 parser.add_argument('-d', '--delay', nargs='?', type=int, dest="delay", default=3, help="Delay between issued ban commands, in seconds. Default:3")
 args = parser.parse_args()
 
@@ -35,14 +34,26 @@ if args.outfile:
    logFile = open(args.outfile, "w+")
 
 for peer in jdata:
-        if  peer["subver"] != args.subver:
-                ipaddr = peer["addr"].split(":")[0]
-                command = args.cli + " setban " + ipaddr + " add " + str(args.seconds)
-                try:
-                    if 'logFile' in locals():
-                        logFile.write(command + "\n")
-                    retcode = subprocess.call(command, shell=True)
-                    sleep(args.delay)   
-                except OSError as e:
-                        print("Execution failed:", e)
+	if args.subver:
+        	if  peer["subver"] != args.subver:
+                	ipaddr = peer["addr"].split(":")[0]
+                	command = args.cli + " setban " + ipaddr + " add " + str(args.seconds)
+                	try:
+                    		if 'logFile' in locals():
+                        		logFile.write(command + "\n")
+                    		retcode = subprocess.call(command, shell=True)
+                    		sleep(args.delay)   
+                	except OSError as e:
+                        	print("Execution failed:", e)
+	if args.protocol:
+		if peer["version"] != int(args.protocol):
+			ipaddr = peer["addr"].split(":")[0]
+                        command = args.cli + " setban " + ipaddr + " add " + str(args.seconds)
+                        try:
+                                if 'logFile' in locals():
+                                        logFile.write(command + "\n")
+                                retcode = subprocess.call(command, shell=True)
+                                sleep(args.delay)
+                        except OSError as e:
+                                print("Execution failed:", e)
 
